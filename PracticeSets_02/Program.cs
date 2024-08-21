2c6f23602ae944639ddeb460082d5621b13f2806
Denjiken <armanvicente4@gmail.com>
2024-08-21 11:45:41 +0800

    done

---
+++ PracticeSets_02/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeSets_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n = 1; n <= 20; n++)
+            {
+                if (n % 2 == 0)
+            }
+            Console.WriteLine("Even Numbers: " + n);
+        }
+    }
+}
