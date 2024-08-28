041194ace0f756a61c2cc3bab96cf059bacb0f12
JohnCarloDagoy <202380282@psu.palawan.edu.ph>
2024-08-28 16:49:06 +0800

    finish

---
+++ PS02_odd/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace PS02_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listing odd numbers between 1 and 20:");
+
+            for (int number = 1; number <= 20; number++)
+            {
+                if (number % 2 != 0)
+                {
+                    Console.WriteLine("Odd number: " + number);
+                }
+            }
+
+            Console.WriteLine("Finished listing all odd numbers.");
+        }
+    }
+}
